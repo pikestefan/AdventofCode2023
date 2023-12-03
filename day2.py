7288b5a385e03e44adffdfd9ff136c01bc8410bb
L <>
2023-12-03 23:21:46 +0100

    Blackened day 2

--- day2.py
+++ day2.py
@@ -19,24 +19,25 @@ filename = f"day{day_num}.txt"
 def part1():
     colors = ["red", "green", "blue"]
     smartint = lambda string: int(string or 0)
-    
-    
+
     with open(probfolder / filename) as file:
         lines = file.readlines()
-        
+
         possible_rounds = 0
         for line in lines:
-            round_dict = dict(zip(colors, [0,0,0]))
+            round_dict = dict(zip(colors, [0, 0, 0]))
             round_id = int(re.match("Game (\d+)", line).group(1))
             for color in colors:
                 result = re.findall(f"(\d+) {color}", line)
                 result = max(map(smartint, result))
 
                 round_dict[color] = result
-                
-            if (round_dict["red"] <= 12 and 
-                round_dict["green"] <= 13 and 
-                round_dict["blue"] <= 14):
+
+            if (
+                round_dict["red"] <= 12
+                and round_dict["green"] <= 13
+                and round_dict["blue"] <= 14
+            ):
                 possible_rounds += round_id
     return possible_rounds
 
@@ -44,12 +45,12 @@ def part1():
 def part2():
     colors = ["red", "green", "blue"]
     smartint = lambda string: int(string or 0)
-    
+
     with open(probfolder / filename) as file:
         lines = file.readlines()
         tot_power = 0
         for line in lines:
-            round_dict = dict(zip(colors, [0,0,0]))
+            round_dict = dict(zip(colors, [0, 0, 0]))
             # round_id = int(re.match("Game (\d+)", line).group(1))
             for color in colors:
                 result = re.findall(f"(\d+) {color}", line)
@@ -58,7 +59,7 @@ def part2():
                 round_dict[color] = result
             power = prod(round_dict.values())
             tot_power += power
-            
+
     return tot_power
 
 
